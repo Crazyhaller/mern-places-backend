6d8b2a7b2b685c846f04020aaac797b569ade772
crazyhaller <suvigyamadrid@gmail.com>
2023-04-14 18:32:25 +0530

    adding error model

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express()
 app.use('/api/places', placesRoutes)
 
 app.use((error, req, res, next) => {
-    if(res.headerSent){
-        return next(error)
-    }
+  if (res.headerSent) {
+    return next(error)
+  }
 
-    res.status(error.code || 500)
-    res.json({message: error.message || 'An unknown error occured!'})
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'An unknown error occured!' })
 })
 
 app.listen(5000)
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,46 +1,47 @@
 const express = require('express')
+const HttpError = require('../models/http-error')
 const router = express.Router()
 
-const DUMMY_PLACES = [{
+const DUMMY_PLACES = [
+  {
     id: 'p1',
     title: 'Empire State Building',
     description: 'One of the most famous sky scrappers in the world.',
     location: {
-        lat: 40.7484445,
-        lng: -73.9882393
+      lat: 40.7484445,
+      lng: -73.9882393,
     },
     address: '20 W 34th St., New York, NY 10001',
-    creator: 'u1'
-}]
+    creator: 'u1',
+  },
+]
 
 router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId
-    })
+  const placeId = req.params.pid
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId
+  })
 
-    if(!place){
-        const error = new Error('Could not find a place for the provided id.')
-        error.code = 404
-        throw error
-    }
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404)
+  }
 
-    res.json({place})
+  res.json({ place })
 })
 
 router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId
-    })
+  const userId = req.params.uid
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId
+  })
 
-    if(!place){
-        const error = new Error('Could not find a place for the provided user id.')
-        error.code = 404
-        return next(error)
-    }
+  if (!place) {
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    )
+  }
 
-    res.json({place})
+  res.json({ place })
 })
 
-module.exports=router
+module.exports = router
