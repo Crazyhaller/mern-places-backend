d55282bcdaa7043ee855195eeb064a7b4da001d2
crazyhaller <suvigyamadrid@gmail.com>
2023-04-24 21:13:04 +0530

    adding the user login

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -24,7 +24,7 @@ const signup = async (req, res, next) => {
     )
   }
 
-  const { name, email, password } = req.body
+  const { name, email, password, places } = req.body
 
   let existingUser
   try {
@@ -67,16 +67,26 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) })
 }
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email)
+  let existingUser
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later',
+      500
+    )
+    return next(error)
+  }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      'Could not find the user, credentials seems to be wrong',
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in',
       401
     )
+    return next(error)
   }
 
   res.json({ message: 'Logged In' })
